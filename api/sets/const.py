8e7a05308a82278dde58f5bdadc105e9990fb913
nil-laptop <nilrey@gmail.com>
2024-08-05 14:46:16 +0300

    upd BLOCK_LIST_IMAGES

--- api/sets/const.py
+++ api/sets/const.py
@@ -8,5 +8,5 @@ CNTR_BASE_01_DIR_OUT="/output"
 PARAM_TO_JSON = " --format '{{json .}}' "
 PARAM_NO_TRUNC = " --no-trunc "
 
-BLOCK_LIST_IMAGES = ['idockerapi', 'vdbr/grafana', 'camino-camino-plugins', 'vdbr/monitor', 'bytetracker-image', 'vdbr/test', 'elestio/pgadmin', 'jrottenberg/ffmpeg', 'yiisoftware/yii2-php', 'postgres', 'inevm/camino', 'grafana/grafana-enterprise', 'postgres', 'prom/node-exporter', 'ubuntu/prometheus', 'gcr.io/cadvisor/cadvisor']
+BLOCK_LIST_IMAGES = ['idockerapi', 'vdbr/grafana', 'camino-camino-plugins', 'vdbr/monitor', 'vdbr/test', 'elestio/pgadmin', 'jrottenberg/ffmpeg', 'yiisoftware/yii2-php', 'postgres', 'inevm/camino', 'grafana/grafana-enterprise', 'postgres', 'prom/node-exporter', 'ubuntu/prometheus', 'gcr.io/cadvisor/cadvisor']
 BLOCK_LIST_CONTAINERS = ['camino-back', 'camino-pgdb', 'camino-restapi', 'camino-front', 'camino-plugins', 'camino-pgadmin', 'node-exporter']
