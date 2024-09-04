76864f489b6197ee3cbdb949b2aeac41b236d0a0
nil-laptop <nilrey@gmail.com>
2024-09-04 16:39:44 +0300

    ImageRun class fix

--- api/lib/reqest_classes.py
+++ api/lib/reqest_classes.py
@@ -3,26 +3,26 @@ from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationErro
 
 class ImageRun(BaseModel):
    name: str = Field(default="")
+   ann_mode:str = Field(default="")
    weights:str = Field(default="")
    hyper_params:str = Field(default="")
    in_dir:str  = Field(default="")
    out_dir:str = Field(default="")
    network:str = Field(default="")
-   ann_mode:str = Field(default="")
-   yolov5s:str = Field(default="")
+   video_storage:str = Field(default="")
    host_web:str = Field(default="camino-api")
    work_time:str = Field(default="")
    
 
    def getAllParams(self):
     return {'name': self.name, 
+            'ann_mode': self.ann_mode, 
             'weights': self.weights, 
             'hyper_params': self.hyper_params, 
             'in_dir': self.in_dir, 
             'out_dir': self.out_dir, 
             'network': self.network, 
-            'ann_mode': self.ann_mode, 
-            'yolov5s': self.yolov5s, 
+            'video_storage': self.video_storage, 
             'host_web': self.host_web, 
             'work_time': self.work_time, 
             }
@@ -35,13 +35,13 @@ class ANNEventBeforeRun(BaseModel):
 
 class ContainerCreate(BaseModel):
    name: str = Field(default="")
+   ann_mode:str = Field(default="")
    weights:str = Field(default="")
    hyper_params:str = Field(default="")
    in_dir:str  = Field(default="")
    out_dir:str = Field(default="")
    network:str = Field(default="")
-   ann_mode:str = Field(default="")
-   yolov5s:str = Field(default="")
+   video_storage:str = Field(default="")
    host_web:str = Field(default="camino-api")
    work_time:str = Field(default="")
    
@@ -49,13 +49,13 @@ class ContainerCreate(BaseModel):
 
    def getAllParams(self):
     return {'name': self.name, 
+            'ann_mode': self.ann_mode, 
             'weights': self.weights, 
             'hyper_params': self.hyper_params, 
             'in_dir': self.in_dir, 
             'out_dir': self.out_dir, 
             'network': self.network, 
-            'ann_mode': self.ann_mode, 
-            'yolov5s': self.yolov5s, 
+            'video_storage': self.video_storage, 
             'host_web': self.host_web, 
             'work_time': self.work_time, 
             }
