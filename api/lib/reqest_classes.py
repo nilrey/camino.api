d6d7157b9a40cc5294ac2d0337912c1d962156b9
nil-laptop <nilrey@gmail.com>
2024-09-04 16:29:14 +0300

    class ImageRun upd

--- api/lib/reqest_classes.py
+++ api/lib/reqest_classes.py
@@ -7,6 +7,12 @@ class ImageRun(BaseModel):
    hyper_params:str = Field(default="")
    in_dir:str  = Field(default="")
    out_dir:str = Field(default="")
+   network:str = Field(default="")
+   ann_mode:str = Field(default="")
+   yolov5s:str = Field(default="")
+   host_web:str = Field(default="camino-api")
+   work_time:str = Field(default="")
+   
 
    def getAllParams(self):
     return {'name': self.name, 
@@ -14,6 +20,11 @@ class ImageRun(BaseModel):
             'hyper_params': self.hyper_params, 
             'in_dir': self.in_dir, 
             'out_dir': self.out_dir, 
+            'network': self.network, 
+            'ann_mode': self.ann_mode, 
+            'yolov5s': self.yolov5s, 
+            'host_web': self.host_web, 
+            'work_time': self.work_time, 
             }
    
 
