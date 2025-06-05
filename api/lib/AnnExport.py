a6858c58fdeed558365ba4aa0760b90222199f8f
nil-laptop <nilrey@gmail.com>
2025-06-05 16:51:19 +0300

    upd ANNExporter: add log message - processing

--- api/lib/AnnExport.py
+++ api/lib/AnnExport.py
@@ -3,6 +3,7 @@ import threading
 import requests 
 import datetime as dt
 import subprocess
+import time
 from pathlib import Path
 from  api.format.logger import LogManager
 import api.sets.config as C
@@ -19,16 +20,16 @@ class ANNExporter:
         self.weights_path = Path(export_data.weights)
         self.export_path = self.base_path / export_data.export
         self.readme_path = Path(f"{C.EXPORT_README_PATH}/{C.EXPORT_README_FNAME}")
-        self.image_tar_path = self.base_path / f"export_image_{get_dt_now_noms_nows()}"
-
+        self.image_tar_path = self.base_path / f"export_img_{export_data.export}_{get_dt_now_noms_nows()}"
+        self.is_archiving = False  
         self.logger = LogManager("ann_export")
         self.logger.info(f"Получены параметры: {export_data}")
 
     def _log(self, message: str):
-        self.logger.info(f"{get_dt_now_noms()} - {message}")
+        self.logger.info(message)
 
     def _log_error(self, message: str):
-        self.logger.error(f"[{self.ann_id}] {message}")
+        self.logger.error(message)
 
     def _send_ann_arch_on_save(self, ann_id):
         template = ENDPOINTS.get("ann_archive_on_save")
@@ -65,10 +66,21 @@ class ANNExporter:
 
     def _create_archive(self):
         self._log(f"Создание архива '{self.export_path.name}'.")
-        with tarfile.open(self.export_path, "w:gz") as tar:
-            tar.add(self.image_tar_path, arcname=self.image_tar_path.name)
-            tar.add(self.weights_path, arcname=self.weights_path.name)
-            tar.add(self.readme_path, arcname=self.readme_path.name)
+        self.is_archiving = True 
+        log_thread = threading.Thread(target=self._log_archiving_progress, daemon=True)
+        log_thread.start()        
+        try:
+            with tarfile.open(self.export_path, "w:gz") as tar:
+                tar.add(self.image_tar_path, arcname=self.image_tar_path.name)
+                tar.add(self.weights_path, arcname=self.weights_path.name)
+                tar.add(self.readme_path, arcname=self.readme_path.name)
+        finally:
+            self.is_archiving = False 
+
+    def _log_archiving_progress(self):
+        while self.is_archiving:
+            self._log("Процесс архивирования запущен")
+            time.sleep(10)
 
     def _delete_tmp_image(self):
         if self.image_tar_path.exists():
